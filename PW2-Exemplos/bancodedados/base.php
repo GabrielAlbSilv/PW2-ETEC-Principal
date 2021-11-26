6d5183ad6d474991ddd840724e818e6fbfab7193
GabrielAlbSilv <gabrielalbuquerquemoura@gmail.com>
2021-11-25 21:45:16 -0300

    nova commit

--- PW2-Exemplos/bancodedados/base.php
+++ PW2-Exemplos/bancodedados/base.php
@@ -1,12 +1,13 @@
 <?php 
 
     //Criando uma conexão com o banco de dados
-    $con = mysqli_connect("localhost","root","");
+    $con = mysqli_connect("localhost","root","", "empresaabc"); //emppresaabc é o banco de dados
 
     //Checando a conexão
     if(mysqli_connect_errno($con)){
 
         echo "Erro ao conectar com a base de dados";
+        mysqli_connect_error();
     }else{
 
         echo"conexão aberta";
